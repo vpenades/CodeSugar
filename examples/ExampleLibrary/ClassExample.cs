90c90d1ebb94415aaca59be5ffb4995b709cfd5d
vpenades <5433822+vpenades@users.noreply.github.com>
2024-01-10 23:26:49 +0100

    added examples

---
+++ examples/ExampleLibrary/ClassExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using ExampleLibrary;
+
+namespace LibraryExample
+{
+    public class ClassExample
+    {
+        public void ExampleOfStreamWrite()
+        {
+            using(var m = new System.IO.MemoryStream())
+            {
+                m.WriteAllBytes(new byte[1024]);
+                m.WriteValue<int>(10);
+                m.WriteValue<float>(0.1f);
+                m.WriteString("hello world");
+            }
+        }
+
+    }
+}
---
+++ examples/ExampleWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        public void ExampleOfWriteAllBytes()
+        {
+            using (var m = new System.IO.MemoryStream())
+            {
+                m.WriteAllBytes(new byte[1024]);
+            }
+        }
+    }
+}
