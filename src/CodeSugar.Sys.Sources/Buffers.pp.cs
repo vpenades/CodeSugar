4dd5d37429bafd01ae25588845189c2310b41748
vpenades <5433822+vpenades@users.noreply.github.com>
2024-05-21 14:23:56 +0200

    added ArrayPool extensions

---
+++ src/CodeSugar.Sys.Sources/Buffers.pp.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) CodeSugar 2024 Vicente Penades
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+#nullable disable
+
+#if CODESUGAR_USECODESUGARNAMESPACE
+namespace CodeSugar
+#elif CODESUGAR_USESYSTEMNAMESPACE
+namespace System
+#else
+namespace $rootnamespace$
+#endif
+{
+    partial class CodeSugarForSystem
+    {
+        #if !NETSTANDARD2_0
+        public static ArraySegment<T> RentSegment<T>(this System.Buffers.ArrayPool<T> pool, int length)
+        {
+            var array = pool.Rent(length);
+            return array == null
+                ? default
+                : new ArraySegment<T>(array, 0, length);
+        }
+
+        public static void ReturnSegment<T>(this System.Buffers.ArrayPool<T> pool, ArraySegment<T> segment)
+        {
+            pool.Return(segment.Array);
+        }
+        #endif
+    }
+}
