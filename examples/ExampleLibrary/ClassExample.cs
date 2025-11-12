fd7ab9c431760cf73e1527421f3a1cd367498622
vpenades <5433822+vpenades@users.noreply.github.com>
2025-11-12 16:11:04 +0100

    updated example

--- examples/ExampleLibrary/ClassExample.cs
+++ examples/ExampleLibrary/ClassExample.cs
@@ -11,8 +11,8 @@ namespace ExampleLibrary
             using(var m = new System.IO.MemoryStream())
             {
                 m.WriteAllBytes(new byte[1024]);
-                m.WriteValue<int>(10);
-                m.WriteValue<float>(0.1f);
+                m.WriteLeS32(10);
+                m.WriteLeF32(0.1f);
                 m.WriteString("hello world");
             }
         }
